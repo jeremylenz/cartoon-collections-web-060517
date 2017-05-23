5e7f7f2bbcb403162e846067696760d031966c40
Jeremy Lenz <jeremylenz@gmail.com>
2017-05-23 18:04:34 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_array)
+  dwarf_array.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planets)
+  planets.each do |planet|
+    planet << "!"
+    planet.capitalize!
+end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  long_names = calls.select do |call|
+    call.length > 4
+  end
+  !long_names.empty?
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(list)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  list.find do |possible_cheese|  #Search thru the list we are given.  See if each element is a type of cheese.  Return the first such element.
+    cheese_types.include?(possible_cheese)
+  end
+
 end
